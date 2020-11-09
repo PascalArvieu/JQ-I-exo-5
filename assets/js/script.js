3b4f3485cd4b7e82d96cb63201aed9342a7baaca
PascalArvieu <pascalarvieu@gmail.com>
2020-11-09 15:03:13 +0100

    JQ exo 6

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(function () {
+	$("#loser").mouseenter(function () {
+		$(this).css("width", "+=100")();
+	});
+	$("#loser").mouseleave(function () {
+		$(this).css("width", "-=50")();
+	});
+});
+
+
+
+// $(function () {
+// 	$("#loser").mouseover(function () {
+// 		$(this).css("width", "300px")();
+// 	});
+// });
+
+
